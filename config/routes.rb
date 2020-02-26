520223fbc4d97cdc82e46dd963343efba99c5da1
Reid Walker <Reid96@gmail.com>
2020-02-26 09:35:55 -0600

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   resources :players, only: :show
   resources :characters, only: :show
   resources :campaigns, only: :show
@@ -8,6 +9,6 @@ Rails.application.routes.draw do
 
   get 'search', to: 'players#search', as: 'search'
   get 'about', to: 'players#about', as: 'about'
+
   root to: 'players#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
